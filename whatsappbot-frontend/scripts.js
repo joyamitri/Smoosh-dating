e8cfe6a5d8d3316120349ba2aed58a597db9b23b
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-12-11 23:57:33 +0200

    Updating <> scripts

--- whatsappbot-frontend/scripts.js
+++ whatsappbot-frontend/scripts.js
@@ -1,69 +1,82 @@
 const pages = {}
+const base_url = "localhost:8000/api"
 
 // ##################################
 //         LOGIN AND SIGNUP
 // ##################################
 let current;
+const username = document.getElementById('username')
+const password = document.getElementById('password')
 
 pages.loadFor = (page) => { 
   eval("pages.load_" + page + "();")
 }
 
-document.getElementById('username').addEventListener('focus', function(e) {
-    if (current) current.pause()
-    current = anime({
-        targets: 'path',
-        strokeDashoffset: {
-        value: 0,
-        duration: 700,
-        easing: 'easeOutQuart'
-        },
-        strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-        }
-    })
-})
+pages.load_login = () =>{
+    const api_url = base_url + "/login"
 
-document.getElementById('password').addEventListener('focus', function(e) {
-    if (current) current.pause()
-    current = anime({
-      targets: 'path',
-      strokeDashoffset: {
-        value: -336,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
+    username.addEventListener('focus', function(e) {
+        if (current) current.pause()
+        current = anime({
+            targets: 'path',
+            strokeDashoffset: {
+            value: 0,
+            duration: 700,
+            easing: 'easeOutQuart'
+            },
+            strokeDasharray: {
+            value: '240 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+            }
+        })
     })
-})
-
-document.getElementById('submit').addEventListener('focus', function(e) {
-    if (current) current.pause()
-    current = anime({
-      targets: 'path',
-      strokeDashoffset: {
-        value: -730,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '530 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
+    
+    password.addEventListener('focus', function(e) {
+        if (current) current.pause()
+        current = anime({
+          targets: 'path',
+          strokeDashoffset: {
+            value: -336,
+            duration: 700,
+            easing: 'easeOutQuart'
+          },
+          strokeDasharray: {
+            value: '240 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+          }
+        })
     })
-})
-
-const form = document.getElementById('sign')
-form.addEventListener('click', function(){
-  
-})
+    
+    document.getElementById('submit').addEventListener('focus', function(e) {
+        if (current) current.pause()
+        current = anime({
+          targets: 'path',
+          strokeDashoffset: {
+            value: -730,
+            duration: 700,
+            easing: 'easeOutQuart'
+          },
+          strokeDasharray: {
+            value: '530 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+          }
+        })
+    })
+    
+    const form = document.getElementById('sign')
+    form.addEventListener('click', async function(){
+        const formData = new FormData()
+        const user = username.value
+        const pass = password.value
+        formData.append("username", user)
+        formData.append("password", pass)
+        const resp = await axios.postAPI(api_url, formData)
+    })
+    
+}
 
 pages.postAPI = async(api_url, api_data, api_token = null) => {  
     try {
