39c22d993b95c31adc4eff46d9a1ffa39dea716d
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-12-11 23:22:02 +0200

    Updating <> scripts

--- whatsappbot-frontend/scripts.js
+++ whatsappbot-frontend/scripts.js
@@ -3,7 +3,25 @@ const pages = {}
 // ##################################
 //         LOGIN AND SIGNUP
 // ##################################
+let current;
 
 pages.loadFor = (page) => { 
   eval("pages.load_" + page + "();")
 }
+
+document.getElementById('username').addEventListener('focus', function(e) {
+  if (current) current.pause()
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  })
+})
