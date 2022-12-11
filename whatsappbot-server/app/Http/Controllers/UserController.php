23c83020281c087649875623a18452a0c8536f64
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-12-11 23:59:09 +0200

    Updating <> UserController

--- whatsappbot-server/app/Http/Controllers/UserController.php
+++ whatsappbot-server/app/Http/Controllers/UserController.php
@@ -6,9 +6,5 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    function login(){
-        return response() -> json([
-            "status"=> "success"
-        ], 404);
-    }
+    
 }
