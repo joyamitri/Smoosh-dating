3dc6dec41c50c4b351d7a7fa3cfb7b8525ec30ca
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-12-16 01:44:06 +0200

    Updating <> api.php

--- whatsappbot-server/routes/api.php
+++ whatsappbot-server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(["prefix" => "user"], function(){
+    Route::post('login', [UserController::class, "login"]);
+});
+
