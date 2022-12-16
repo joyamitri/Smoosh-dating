76b558afc54b7c19e9c158331e723c476cf13a22
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-12-16 16:52:58 +0200

    Updating <> config

--- whatsappbot-server/config/auth.php
+++ whatsappbot-server/config/auth.php
@@ -14,8 +14,10 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
+        // 'guard' => 'web',
+        // 'passwords' => 'users',
     ],
 
     /*
@@ -39,6 +41,11 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
