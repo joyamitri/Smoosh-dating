634ca55ef2a1e46b5b1975e24ba7534c618c4c2c
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-12-19 23:20:36 +0200

    Updating <> scripts

--- smoosh-frontend/scripts.js
+++ smoosh-frontend/scripts.js
@@ -262,3 +262,20 @@ smoosh.load_landing = () => {
   })
 
 }
+
+// ##################################
+//         BROWSE PAGE
+// ##################################
+
+let swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
